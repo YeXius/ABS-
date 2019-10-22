f63fa044665b3761b811ec88bcdaf5e8add736fd
常松 <1164094280@qq.com>
2019-10-22 20:23:43 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <!-- <ul>
-      <li v-for='(item,index) in arr' :key="index">
-        <img :src="item" alt="">
-      </li>
-    </ul> -->
   </div>
 </template>
 
@@ -15,24 +10,6 @@ export default {
   name: 'app',
   components: {
 
-  },
-  created(){
-    this.getData();
-  },
-  data(){
-    return {
-      arr:[]
-    }
-  },
-  methods:{
-    getData(){
-      // console.log(this.$http)
-      this.$http.get('http://www.abs.com/home')
-        .then(res=>{
-          console.log(res)
-          // this.arr=res.data.lunbo
-        })
-    }
   }
 }
 </script>
